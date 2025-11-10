6417d1565c32370fef3c0b34ac92d849b054d378
Kaemo00 <Kaemly20@icloud.com>
2025-11-09 20:38:28 -0500

    chore(deploy): add gh-pages devDependency and Vite base alias for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kaemly/', // important pour GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/Kaemly/', // Pour GitHub Pages
 });
